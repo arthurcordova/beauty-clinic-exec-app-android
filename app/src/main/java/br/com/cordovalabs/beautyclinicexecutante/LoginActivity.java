ce68ddba4d8b6c1849e4cf9ad3ce7e9ab7b02c2f
arthurcordova <arthur.stapassoli@gmail.com>
2017-03-30 22:23:50 -0300

    Main view

--- app/src/main/java/br/com/cordovalabs/beautyclinicexecutante/LoginActivity.java
+++ app/src/main/java/br/com/cordovalabs/beautyclinicexecutante/LoginActivity.java
@@ -1,5 +1,6 @@
 package br.com.cordovalabs.beautyclinicexecutante;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,17 +16,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
             }
         });
+
     }
 
     @Override
